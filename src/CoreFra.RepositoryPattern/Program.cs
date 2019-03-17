b013b262c6e6dc40afe4c86cf49c273d35f31bd4
behzadrezaie11 <behzadrezaie11@gmail.com>
2019-03-17 11:55:00 +0330

    First commit

---
+++ src/CoreFra.RepositoryPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreFra.RepositoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
